8c63128bf5035d5183675f0f5daac9270254dc24
Archit Rathi <64769555+arath089@users.noreply.github.com>
2020-06-09 13:45:54 -0600

    Update Pipeling and Gating.v

--- Pipeling and Gating.v
+++ Pipeling and Gating.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/14/2020 11:42:34 AM
-// Design Name: 
-// Module Name: assss2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
 
